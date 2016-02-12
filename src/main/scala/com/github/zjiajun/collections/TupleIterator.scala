1ab9c035604e2c745cacd3007b3977bd7c4d2d38
zhujiajun <zhujiajun.me@gmail.com>
2016-02-12 21:52:46 +0800

    TupleIterator

---
+++ src/main/scala/com/github/zjiajun/collections/TupleIterator.scala
@@ -0,0 +1,17 @@
+package com.github.zjiajun.collections
+
+/**
+  * Created by zhujiajun
+  * 16/2/12 21:41
+  *
+  * Tuple(元组)迭代,继承了Product特质,使用productIterator进行迭代
+  */
+object TupleIterator extends App {
+
+  val tupleValue = (1,2,3,4,5,6)
+
+  def tupleIterator(tuple: Product): Unit = tuple.productIterator.foreach(println)
+
+  tupleIterator(tupleValue)
+
+}
