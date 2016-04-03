cc002af477737dbb895e787e7a1c505ca6ee27a4
zhujiajun <zhujiajun.me@gmail.com>
2016-04-03 19:53:32 +0800

    Person

---
+++ src/main/scala/com/github/zjiajun/impatient/charpter8/Person.scala
@@ -0,0 +1,23 @@
+package com.github.zjiajun.impatient.charpter8
+
+import java.lang.System
+
+/**
+  * Created by zhujiajun
+  * 16/4/3 19:44
+  */
+class Person(name: String, age: Int) {
+  override def toString: String = getClass.getName + s":{name:$name,age:$age}"
+}
+
+class Employee(name: String, age: Int, salary: Double) extends Person(name, age) {
+  override def toString: String = super.toString + getClass.getName + s":{name:$name,age:$age,salary:$salary}"
+}
+
+object Test extends App {
+
+  val employee = new Employee("e1",30,1234.56)
+  println(employee)
+}
+
+
