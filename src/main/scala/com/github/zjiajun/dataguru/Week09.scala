c0d4ea18639856d1a89bfaf669e98c0bbe03b4df
zhujiajun <zhujiajun.me@gmail.com>
2016-05-21 22:12:13 +0800

    Week09

--- src/main/scala/com/github/zjiajun/dataguru/Week09.scala
+++ src/main/scala/com/github/zjiajun/dataguru/Week09.scala
@@ -13,29 +13,25 @@ object Week09 extends App {
   /*
    * 姓名 -> (年龄,标示,年级,分数)
    */
-  val school = collection.mutable.Map[String,(Int, String, Int, Int)](
+  val school = Map[String,(Int, String, Int, Int)](
     "Teacher_1" -> (30, "T", 0, 0),
-    "Teacher_2" -> (35, "T", 0 ,0),
-    "Teacher_3" -> (40, "T", 0 ,0),
-    "Student_1" -> (18, "S", 3, 100),
-    "Student_2" -> (19, "S", 3, 90),
-    "Student_3" -> (20, "S", 3, 80)
+    "Student_1" -> (18, "S", 3, 100)
   )
 
   case class CRUDSchool(
        flag: String,
-       schoolMap: collection.mutable.Map[String,(Int, String, Int, Int)],
+       schoolMap: Map[String,(Int, String, Int, Int)],
        key: String,
        value: (Int, String, Int, Int))
 
   case class saveSchool(
-         schoolMap: collection.mutable.Map[String,(Int, String, Int, Int)])
+         schoolMap: Map[String,(Int, String, Int, Int)])
 
-  val system = ActorSystem("school")
+  val system = ActorSystem("schoolSystem")
   val saveFileActor = system.actorOf(Props(classOf[SaveFileActor],System.getProperty("user.home")),"saveFileActor")
   val crudActor = system.actorOf(Props(classOf[CRUDActor],saveFileActor),"crudActor")
-  crudActor ! CRUDSchool("D",school,"Teacher_1",null)
-  Thread sleep 2000
+  crudActor ! CRUDSchool("C",school,"Student_2",(19,"S",3,90))
+  Thread sleep 1000
   system.terminate()
 
 
@@ -43,18 +39,20 @@ object Week09 extends App {
 
     override def receive = {
 
-      case CRUDSchool(flag,schoolMap,key,value) => flag match {
-        case "C" =>
-          log.info(s"Add map key : $key, value : $value")
-          schoolMap += (key -> value)
-        case "U" =>
-          log.info(s"Update map key : $key, value : $value")
-          schoolMap(key) = value
-        case "D" =>
-          log.info(s"Delete map key : $key, value : $value")
-          schoolMap -= key
-      }
-        saveFileActor ! saveSchool(schoolMap)
+      case CRUDSchool(flag,schoolMap,key,value) =>
+        saveFileActor ! saveSchool(
+          flag match {
+            case "C" =>
+              log.info(s"Add map key : $key, value : $value")
+              schoolMap + (key -> value)
+            case "U" =>
+              log.info(s"Update map key : $key, value : $value")
+              schoolMap + (key -> value)
+            case "D" =>
+              log.info(s"Delete map key : $key, value : $value")
+              schoolMap - key
+          }
+        )
     }
   }
 
