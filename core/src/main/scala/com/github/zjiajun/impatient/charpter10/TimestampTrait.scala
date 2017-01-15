6de4de94e4abc5e0cf0fdabbb9a8823d0b77d4e4
zhujiajun <zhujiajun.me@gmail.com>
2017-01-15 13:12:20 +0800

    add

---
+++ core/src/main/scala/com/github/zjiajun/impatient/charpter10/TimestampTrait.scala
@@ -0,0 +1,30 @@
+package com.github.zjiajun.impatient.charpter10
+
+/**
+  * @author zhujiajun
+  * @since 2017/1/15
+  */
+trait TimestampTrait extends Logged {
+
+  override def log(msg: String): Unit = super.log(new java.util.Date() + " " + msg)
+
+}
+trait ShortTrait extends Logged {
+  val maxLength = 10
+
+  override def log(msg: String): Unit =
+    super.log(if (msg.length <= maxLength) msg else msg.substring(0, maxLength - 3) + "...")
+
+}
+
+object TestMain extends App {
+
+  //Short 先执行，然后super.log调用Timestamp
+  val s1 = new Saving with ConsoleLogged with  TimestampTrait with ShortTrait
+  //Timestamp 先执行，然后super.log调用short
+  val s2 = new Saving with ConsoleLogged with ShortTrait with TimestampTrait
+
+  s1.save(1)
+  s2.save(2)
+
+}
