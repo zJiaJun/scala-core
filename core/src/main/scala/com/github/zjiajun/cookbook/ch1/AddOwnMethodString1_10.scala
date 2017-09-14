1064561a5a707f366c2bda4e2771c822f3525cfe
zhujiajun <zhujiajun.me@gmail.com>
2017-09-14 11:31:56 +0800

    cookbook

--- core/src/main/scala/com/github/zjiajun/cookbook/ch1/AddOwnMethodString1_10.scala
+++ core/src/main/scala/com/github/zjiajun/cookbook/ch1/AddOwnMethodString1_10.scala
@@ -13,6 +13,12 @@ object AddOwnMethodString1_10 extends App {
 
     def hideAll: String = s.replaceAll(".", "*")
 
+    def plusOne: Int = s.toInt + 1
+
+    def asBoolean: Boolean = s match {
+      case "0" | "zero" | "" | " " => false
+      case _ => true
+    }
   }
 
   val s = "HAL".increment
@@ -21,5 +27,10 @@ object AddOwnMethodString1_10 extends App {
   println(s2)
   val s3 = "HELLO".hideAll
   println(s3)
-
+  val s4 = "1".plusOne
+  println(s4)
+  val s5 = "1".asBoolean
+  println(s5)
+  val s6 = "0".asBoolean
+  println(s6)
 }
