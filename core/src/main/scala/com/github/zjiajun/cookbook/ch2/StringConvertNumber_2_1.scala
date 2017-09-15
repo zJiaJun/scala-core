362ca33cd1d9ccc901745d438a0ad1ae49040306
zhujiajun <zhujiajun.me@gmail.com>
2017-09-15 22:13:49 +0800

    cookbook

---
+++ core/src/main/scala/com/github/zjiajun/cookbook/ch2/StringConvertNumber_2_1.scala
@@ -0,0 +1,14 @@
+package com.github.zjiajun.cookbook.ch2
+
+/**
+  * @author zhujiajun
+  * @since 2017/9/15
+  */
+object StringConvertNumber_2_1 extends App {
+
+  val a = BigInt("1")
+  println(a)
+
+  val b = BigDecimal("3.13459")
+  println(b)
+}
