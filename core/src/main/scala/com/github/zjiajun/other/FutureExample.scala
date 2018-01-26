f3b49a4ec6635e49a6b15f45aefbe91c131c9ac0
zhujiajun <zhujiajun.me@gmail.com>
2018-01-26 22:35:54 +0800

    routing

--- core/src/main/scala/com/github/zjiajun/other/FutureExample.scala
+++ core/src/main/scala/com/github/zjiajun/other/FutureExample.scala
@@ -1,6 +1,7 @@
 package com.github.zjiajun.other
 
 import scala.concurrent.Future
+import scala.io.StdIn
 import scala.util.{Failure, Success}
 
 /**
@@ -13,16 +14,26 @@ object FutureExample extends App {
 
    val future: Future[String] = Future {
     println("begin future")
-    Thread.sleep(2)
+    Thread.sleep(1000)
     println("end future")
     "return future value"
   }
+
   println(future.isCompleted)
 
-  future.onComplete{
+  future.onComplete {
     case Success(v) => println(v)
     case Failure(e) => println(e)
   }
 
+  val f1: Future[Int] = Future { println("f1"); Thread sleep 2000; 1}
+  val f2: Future[Int] = Future { println("f2"); Thread sleep 2000; 2}
+
+  val s: Future[String] = for { v1 <- f1
+        v2 <- f2 } yield s"result $v1 + $v2"
+
+  println(s)
+  Thread sleep 3000
+  println(s.value)
 
 }
