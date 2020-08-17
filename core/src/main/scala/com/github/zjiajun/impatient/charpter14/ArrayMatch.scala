c9dd1534c2f0a59f0a8c0f115ca7a81d5057eac9
zjiajun <zhujiajun.me@gmail.com>
2020-08-17 23:34:36 +0800

    添加log4j2

--- core/src/main/scala/com/github/zjiajun/impatient/charpter14/ArrayMatch.scala
+++ core/src/main/scala/com/github/zjiajun/impatient/charpter14/ArrayMatch.scala
@@ -1,21 +1,23 @@
 package com.github.zjiajun.impatient.charpter14
 
+import org.apache.logging.log4j.scala.Logging
+
 /**
  * @author zhujiajun
  * @version 1.0
  * @since 2020/8/17 22:54
  */
-object ArrayMatch extends App {
+object ArrayMatch extends App with Logging {
 
   val array = Array(0,10,11,12)
 
   def arrayMatch(array: Array[Int]): Unit = {
     array match {
-      case Array(0) => println("0")
-      case Array(x,y) => println(s"$x, $y")
+      case Array(0) => logger.info("0")
+      case Array(x,y) => logger.info(s"$x, $y")
       case Array(0, rest @ _*) => {
-        println("0 ...")
-        println(rest.seq)
+        logger.info("0 ...")
+        logger.info(rest.seq)
       }
     }
   }
