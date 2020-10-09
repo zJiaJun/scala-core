0ea1c999095f48213ae4907bdccac5b16c683889
zjiajun <zhujiajun.me@gmail.com>
2020-10-09 21:53:01 +0800

    AutoSessionExample

--- scalike/src/main/scala/com/github/zjiajun/scalike/AutoSessionExample.scala
+++ scalike/src/main/scala/com/github/zjiajun/scalike/AutoSessionExample.scala
@@ -5,28 +5,28 @@ import com.github.zjiajun.scalike.entity._
 import scalikejdbc._
 
 /**
- * @author zhujiajun
- * @version 1.0
- * @since 2020/10/9 19:51
- *
- * @see [[http://scalikejdbc.org/documentation/auto-session.html]]
-*/
+  * @author zhujiajun
+  * @version 1.0
+  * @since 2020/10/9 19:51
+  *
+  * @see [[http://scalikejdbc.org/documentation/auto-session.html]]
+  */
 object AutoSessionExample extends App with LazyLogging {
 
   initHikariConnectionPool()
-  DB readOnly {implicit session =>
+  DB readOnly { implicit session =>
     findById(1L) // 适用session ActiveSession
   }
   findById(2L) //使用默认session AutoSession 或者 ReadOnlyAutoSession
 
-  def findById(id: Long)(implicit session: DBSession = ReadOnlyAutoSession): Option[Member] = {
+  def findById(id: Long)(implicit session: DBSession = AutoSession): Option[Member] = {
     logger.info(s"findById using session, $session")
-      val memberOpt = sql"select * from members where id = $id"
-        .map(rs => Member(rs.long(1), rs.string(2), rs.date(3)))
-        .single().apply()
+    val memberOpt = sql"select * from members where id = $id"
+      .map(rs => Member(rs.long(1), rs.string(2), rs.date(3)))
+      .single()
+      .apply()
     logger.info(s"findById, $memberOpt")
     memberOpt
   }
-
 
 }
--- scalike/src/main/scala/com/github/zjiajun/scalike/TransactionExample.scala
+++ scalike/src/main/scala/com/github/zjiajun/scalike/TransactionExample.scala
@@ -5,14 +5,13 @@ import java.sql.SQLException
 import com.typesafe.scalalogging.LazyLogging
 import scalikejdbc._
 
-
 /**
- * @author zhujiajun
- * @version 1.0
- * @since 2020/10/9 12:45
- *
- * @see [[http://scalikejdbc.org/documentation/transaction.html]]
-*/
+  * @author zhujiajun
+  * @version 1.0
+  * @since 2020/10/9 12:45
+  *
+  * @see [[http://scalikejdbc.org/documentation/transaction.html]]
+  */
 object TransactionExample extends App with LazyLogging {
 
   initHikariConnectionPool()
@@ -29,7 +28,7 @@ object TransactionExample extends App with LazyLogging {
     logger.info(s"usingReadOnly, $nameOpt")
   }
 
-  def usingReadOnlyExecuteUpdateThrowException(): Unit = {
+  def usingReadOnlyExecuteUpdateThrowException(): Unit =
     DB readOnly { implicit session =>
       try {
         sql"update members set name='something' where id = 1".update().apply()
@@ -37,24 +36,22 @@ object TransactionExample extends App with LazyLogging {
         case ex: SQLException => logger.error("usingReadOnlyExecuteUpdateThrowException", ex)
       }
     }
-  }
 
-  def usingAutoCommit(): Unit = {
+  def usingAutoCommit(): Unit =
     DB autoCommit { implicit session =>
       sql"update members set name='Alice' where id = 1".update().apply()
       sql"update members set name='something' where id = 2".update().apply()
     }
-  }
 
-  def usingLocalTx(): Unit = {
-    DB localTx  { implicit session =>
+  def usingLocalTx(): Unit =
+    DB localTx { implicit session =>
       //事务开始
       sql"update members set name='Alice_tx' where id = 1".update().apply()
       sql"update members set name='Bob_tx' where id = 2".update().apply()
       //事务结束
     }
-  }
 
+  //withIn,在已存在的事务中执行, 事务的操作,如 begin, commit, rollback需要使用者自己维护
   def usingWithInTx(): Unit = {
     val db = DB(ConnectionPool.borrow())
     db.begin()
@@ -73,6 +70,5 @@ object TransactionExample extends App with LazyLogging {
       db.close()
     }
   }
-
 
 }
