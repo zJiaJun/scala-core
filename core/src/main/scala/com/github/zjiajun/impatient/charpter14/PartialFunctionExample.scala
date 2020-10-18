603eb6727c44fda7c5e057f8a6caeadbe3274439
zjiajun <zhujiajun.me@gmail.com>
2020-10-18 20:13:25 +0800

    PartialFunctionExample

--- core/src/main/scala/com/github/zjiajun/impatient/charpter14/PartialFunctionExample.scala
+++ core/src/main/scala/com/github/zjiajun/impatient/charpter14/PartialFunctionExample.scala
@@ -12,11 +12,32 @@ import com.typesafe.scalalogging.LazyLogging
   */
 object PartialFunctionExample extends App with LazyLogging {
 
-  val fun: PartialFunction[Char, Int] = { case '+' => 1; case '-' => -1 }
-  val result = fun('+') //调用fun.apply
-  logger.info(s"$result")
-  val isDefined = fun.isDefinedAt('*')
-  logger.info(s"$isDefined")
-  fun('*')
+  simplePartialFunction()
+  catchStatementFunction("a")
+
+  //被包在花括号内的一组case语句是一个偏函数
+  def simplePartialFunction(): Unit = {
+    val fun: PartialFunction[Char, Int] = {
+      case '+' => 1;
+      case '-' => -1
+    }
+    val result = fun('+') //调用fun.apply
+    logger.info(s"simplePartialFunction, $result")
+    val isDefined = fun.isDefinedAt('*')
+    logger.info(s"simplePartialFunction, $isDefined")
+    // fun('*') //抛出MatchError异常
+  }
+
+  // try语句的catch子句是一个偏函数
+  def catchStatementFunction(str: String): Unit = {
+
+    //fun call-by-name 换名调用参数
+    def tryCatch[T](fun: => T, catcher: PartialFunction[Throwable, T]) = try { fun } catch catcher
+
+    val result = tryCatch(str.toInt, {
+      case ex: NumberFormatException => -1
+    })
+    logger.info(s"catchStatementFunction, $result")
+  }
 
 }
